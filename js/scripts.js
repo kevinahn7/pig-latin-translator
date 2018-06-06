223b4805482cf8fe126b974732acd35e2479838d
Kevin Ahn and Scott Fraser <scott@>
2018-06-06 11:17:26 -0700

    add q and vowel functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-function pigLatin(speak) {
+function letterY(speak) {
   var letter = speak.split("").shift();
   var latinWord = speak.split("").splice(1);
   latinWord.push(letter);
-  var theWord = latinWord.join('');
+  var theWord = latinWord.join('') + "ay";
 
 
   return theWord;
 };
+
+function letterQ(speak) {
+  var word = speak.split("");
+  var letters = word.splice(0,2);
+  word.push(letters.join(""))
+  var theWord = word.join("") + "ay";
+  return theWord;
+
+}
+
+function vowelOne(speak){
+  var theWord = speak + 'way';
+  return theWord;
+}
 
 
 
@@ -14,15 +28,23 @@ $(document).ready(function() {
   $("form#pigPlugin").submit(function(event){
     event.preventDefault();
     var sentence = $("input#sentenceInput").val();
-    var newWord = "";
+    var newWord = [];
     var vowels = ['a','e','i','o','u', 'y'];
     var consonants = [ 'b','c','d','f','g','h','j','k','l','m','n','p','qu','r','s','t','v','w','x','z']
     var words = sentence.split(" ");
     var array = []
 
     for (var x=0; x<words.length; x++) {
-      if (words[x][0].toLowerCase() === "y") {
-        var newWord = pigLatin(words[x]);
+      if (words[x].toLowerCase() === '1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0') {      
+      }
+        else if (words[x][0].toLowerCase() === "y") {
+        newWord[x] = letterY(words[x]);
+      } else if (words[x][0] === 'q' && words[x][1] === 'u') {
+        newWord[x] = letterQ(words[x]);
+      } else if (words[x][0].toLowerCase() === 'a'||'e'||'i'||'o'||'u') {
+        console.log('yes');
+        newWord[x] = vowelOne(words[x]);
+
       }
     };
 
@@ -34,7 +56,7 @@ $(document).ready(function() {
 
 
 
-    $('#result').text(newWord);
+    $('#result').text(newWord.join(" "));
 
 
   });
